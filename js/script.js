89c58b3c913fc3b915869aa4679923c8d62dd6b3
Davinder Singh <davindersaini003@gmail.com>
2019-10-22 22:03:57 +0530

    gui

--- js/script.js
+++ js/script.js
@@ -1,1 +1,24 @@
 // maintain state of all the nine boxes ,those who have selected already should not be avialable for selection again.
+
+
+
+/* using min-max algo for computer */
+
+
+// 1) select data structure 
+let ttt = [[' ',' ','o'], ['x',' ','x'], ['o','x','o']];
+const dis = function() {
+    console.log(`${ttt[0][0]} | ${ttt[0][1]} | ${ttt[0][2]} `);
+    console.log(`${ttt[1][0]} | ${ttt[1][1]} | ${ttt[1][2]} `);
+    console.log(`${ttt[2][0]} | ${ttt[2][1]} | ${ttt[2][2]} `);
+};
+dis();
+
+
+// 2) write the min-max algo logic
+
+const turnx = function minmax(tt) {
+    
+}
+
+
