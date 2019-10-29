1b9acfe4bf1e6fa71ebc61768b16b35e807e3da4
Davinder Singh <davindersaini003@gmail.com>
2019-10-29 22:00:35 +0530

    implementing ai

--- js/script.js
+++ js/script.js
@@ -7,6 +7,7 @@
 
 // 1) select data structure 
 let ttt = [[' ',' ','o'], ['x',' ','x'], ['o','x','o']];
+let currentPlayer = 'X';  // it is a flag representing active player(from setting we can set/change the default player)
 const dis = function() {
     console.log(`${ttt[0][0]} | ${ttt[0][1]} | ${ttt[0][2]} `);
     console.log(`${ttt[1][0]} | ${ttt[1][1]} | ${ttt[1][2]} `);
@@ -15,10 +16,29 @@ const dis = function() {
 dis();
 
 
-// 2) write the min-max algo logic
+// 3) check who won the match
 
-const turnx = function minmax(tt) {
+const check = arr => {
+    // 1) check rows
+    for(let i = 0; i < 3; i++){
+       if(arr[i][0] === arr[i][1] && arr[i][1] === arr[i][2]){
+            if(arr[i][0] !== ' ') return arr[i][0];
+        }
+    }
     
-}
-
-
+    // 2) check cols
+    for(let i = 0; i < 3; i++){
+       if(arr[0][i] === arr[1][i] && arr[1][i] === arr[2][i]){
+            if(arr[0][i] !== ' ') return arr[0][i];
+        }
+    }
+    
+    // 3) check diagonals
+    if((arr[0][0] === arr[1][1] && arr[1][1] === arr[2][2])||(arr[0][2] === arr[1][1] && arr[1][1] === arr[2][0])) 
+    {
+        if(arr[i][0] !== ' ') return arr[1][1];                   
+    }
+    
+    // 4) No winnig condition
+    return -1;
+};
