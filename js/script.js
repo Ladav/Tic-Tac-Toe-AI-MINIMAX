d590d88522b3a1e2117220e5774165e2420da7c5
Davinder Singh <davindersaini003@gmail.com>
2019-11-24 22:17:01 +0530

    AI implemented

--- js/script.js
+++ js/script.js
@@ -1,14 +1,7 @@
 /**
- * 1.pending try async await on 224
+ * 1.pending try async await at setTimeout (inside getUserInput there is isMsgActivity is used remove it try async await)
  */
-
-
-
-// maintain state of all the nine boxes ,those who have selected already should not be avialable for selection again.
-// implement minimax and (user againt user) in different files and import that files
-
-
-/* using min-max algo for computer */
+/* when AI vs human - min-max algo for AI */
 
 
 //***************************************************//
@@ -16,8 +9,8 @@
 //***************************************************// 
 const logicController = (() => {
     const matrix = [new Array(3),new Array(3),new Array(3)];
-
     let playerActive = 'X';  // it is a flag representing active player(from setting we can set/change the default player)
+    const vsAI = true;
 
     const resetDataStructure = () => {
         for(let i = 0; i < 3; i++){
@@ -25,6 +18,7 @@ const logicController = (() => {
                 matrix[i][j] = ' ';
             }
         }
+        playerActive = 'X';  // testing when ai vs user then user will play the first move always
     };
 
     const scrapeData = (data) => {
@@ -42,6 +36,28 @@ const logicController = (() => {
         return {row, col};
     };
 
+    const checkMatrix = (curMatrix = matrix) => {
+        // 1) check rows
+        for(let i = 0; i < 3; i++) {
+            if(curMatrix[i][0] === curMatrix[i][1] && curMatrix[i][1] === curMatrix[i][2]) {
+                if(curMatrix[i][0] !== ' ') return curMatrix[i][0];
+            }
+        }
+        // 2) check cols
+        for(let i = 0; i < 3; i++) {
+            if(curMatrix[0][i] === curMatrix[1][i] && curMatrix[1][i] === curMatrix[2][i]) {
+                if(curMatrix[0][i] !== ' ') return curMatrix[0][i];
+            }
+        }
+        // 3) check diagonals
+        if((curMatrix[0][0] === curMatrix[1][1] && curMatrix[1][1] === curMatrix[2][2])||(curMatrix[0][2] === curMatrix[1][1] && curMatrix[1][1] === curMatrix[2][0])) 
+        {
+            if(curMatrix[1][1] !== ' ') return curMatrix[1][1];                   
+        }
+        // 4) No winnig condition
+        return undefined;
+    };
+
     const isMatrixFull = () => {
         for(let i = 0; i < 3; i++){
             for(let j = 0; j < 3; j++){
@@ -53,42 +69,97 @@ const logicController = (() => {
         return true;
     };
 
-    return{
-            // Initialize matrix
-            resetDS: () => { resetDataStructure(); },        
+    const getAvailFields = (currentMatrix) => {
+        let emptyFields = []; // ex- [{row,col},{rol,col}]
+        for(let i = 0; i < 3; i++){
+            for(let j = 0; j < 3; j++){
+                if(currentMatrix[i][j] === ' ') {
+                    emptyFields.push({row: i, col: j});
+                }
+            }
+        }
+        return emptyFields;
+    };
+
+    // let O- aiPlayer AND X- humanPlayer
+    const minimax = (newMatrix, player = 'O') => {
+        // check if there are empty fields in Matrix and return index of fields empty ex- [{row,col},{rol,col}]
+        const availFields = getAvailFields(newMatrix);
+
+        //check if any of the player's won(if AI won return 10, otherwise -10) or if it is tie
+        if(checkMatrix(newMatrix) === 'O') {
+            return { score: 10 };
+        } else if(checkMatrix(newMatrix) === 'X') {
+            return { score: -10 };
+        } else if(availFields.length === 0) {
+            return { score: 0 }; // it's a draw
+        }
+
+        let moves = []; //collect the move's in the current level of tree for comparision
+
+        for(let i = 0; i < availFields.length; i++) {
+            let move = {}; // to store the score and index of the current move {score:number,index:{row,col}}
+
+            move.index = availFields[i];   // it return and store index of an object ex-{row, col}
+            
+            newMatrix[move.index.row][move.index.col] = player; // making the move for the current player
+
+            // calling the minimax again, this time with the second player and pass the updated newMatrix
+            if(player === 'O') {    // O- aiPlayer AND X- humanPlayer
+                let result = minimax(newMatrix, 'X');   // result object get the returned score values from minimax calculation done at lower level from the current level in the tree
+                move.score = result.score;
+            } else {
+                let result = minimax(newMatrix, 'O');     
+                move.score = result.score;
+            }
+
+            newMatrix[move.index.row][move.index.col] = ' ';    // resets newMatrix to what it was before
+            
+            moves.push(move);   // storing the results of current move, for comparing and finding best one  
+        }
+
+        // after going through all possible move's for the current state of newMatrix, now compare the result's (moves Array) and return the best move to one level up of the current level in the tree
+        let bestMove;
+        // choose the highest(max) score in case of aiPlayer and lowest(min) in case of human player
+        if(player === 'O') {    // O- aiPlayer AND X- humanPlayer
+            let bestScore = -10000;    // in case in aiPlayer we want highest score possible
+            for(let i = 0; i < moves.length; i++) {
+                if(bestScore < moves[i].score) {
+                    bestScore = moves[i].score;
+                    bestMove = i;   // selecting the move with highest score, this move will passed to one level up
+                }
+            }
+        }
+        else {
+            let bestScore = 10000;
+            for(let i = 0; i < moves.length; i++){
+                if(bestScore > moves[i].score) {    // this time looking for lowest score
+                    bestScore = moves[i].score;
+                    bestMove = i;
+                }
+            }
+        }
         
+        return moves[bestMove];    // returning the best move of current Matrix to the previous states matrix or simply return back to one level up
+    };
+
+    return{
+            vsAI,   // flag about if the game is player vs AI
+
+            // Initialize matrix
+            resetDS: () => { resetDataStructure(); },
+
+            // check if the matrix is fully filled
+            isFull: () => isMatrixFull(),
+            
             // check if Active player is the winner
-            checkMatrix: () => {
-                // 1) check rows
-                for(let i = 0; i < 3; i++) {
-                    if(matrix[i][0] === matrix[i][1] && matrix[i][1] === matrix[i][2]) {
-                        if(matrix[i][0] !== ' ') return matrix[i][0];
-                    }
-                }
-                
-                // 2) check cols
-                for(let i = 0; i < 3; i++) {
-                    if(matrix[0][i] === matrix[1][i] && matrix[1][i] === matrix[2][i]) {
-                        if(matrix[0][i] !== ' ') return matrix[0][i];
-                    }
-                }
-                
-                // 3) check diagonals
-                if((matrix[0][0] === matrix[1][1] && matrix[1][1] === matrix[2][2])||(matrix[0][2] === matrix[1][1] && matrix[1][1] === matrix[2][0])) 
-                {
-                    if(matrix[1][1] !== ' ') return matrix[1][1];                   
-                }
-                
-                // 4) No winnig condition
-                return undefined;
-            },
-             
+            checkWinner: () => checkMatrix(),
+
             userInput: (dataString) => {
                 let data;
                 
                 // scraping the required data from the dataString
                 data = scrapeData(dataString);
-
                 //updating matrix
                 matrix[data.row][data.col] = playerActive;
             },
@@ -96,11 +167,20 @@ const logicController = (() => {
             // Active Player
             toggleActive:  () => { playerActive = playerActive === 'O' ? 'X' : 'O'; },
 
-            // check if the matrix is fully filled
-            isFull: () => { return (isMatrixFull()); },
-
             // Active User/player
-            getPlayerActive: () => playerActive
+            getPlayerActive: () => playerActive,
+
+            // aiPlayer
+            aiPlayer: () => {
+                let move = {};    //{score:number, index:{row, col}}
+                move = minimax(matrix);
+
+                const row = move.index.row;
+                const col = move.index.col;
+                matrix[row][col] = 'O';    // AI playing as 'O'
+
+                return {row, col};
+            }        
     };
 })();
 
@@ -130,16 +210,15 @@ const UIController = (() => {
     };
 
     const setPlayerActive = () => {
-        DOMInput.X.classList.toggle('active');
-        DOMInput.O.classList.toggle('active');
+            DOMInput.X.classList.toggle('active');
+            DOMInput.O.classList.toggle('active');
     };
 
     // this check if there is any msg being displayed on the screen, return's true or false
     const anyActiveMsg = () => DOMInput.winWindow.style.display === 'block';
 
     return {
-        // reset the game's UI
-        resetUI : (activePlayer) => {
+        resetUI : (activePlayer) => {   // reset the game's UI
             // clean the Matrix
             Array.from(DOMInput.allFields).forEach(field => field.textContent = ' ');
 
@@ -149,42 +228,42 @@ const UIController = (() => {
             // remove the congrats message from the previous gameplay
             DOMInput.winWindow.style.display = 'none';
         },
-
-        // updating UI against user select
-        updateField : (field, player) => {
+        
+        updateField : (field, player) => {   // updating UI against user select
             field.textContent = player;
         },
 
-        updateActivePlayer : (activePlayer) => {
-            console.log(activePlayer);
+        updateActivePlayer : (activePlayer) => {   // when X vs O
+                    // pending - when new game start ex- X vs O after a match of ex- X vs AI, the textContent ('AI') have to revert back to O
             setPlayerActive();
         },
 
-        renderWinner: (activePlayer) => {
-            console.log(anyActiveMsg());
-            // check if any msg is already being displaced on the screen
-           // if(!anyActiveMsg()) { ----- testing
-                // 1-upadate the activePlayer in the html 
-                DOMInput.winner.textContent = activePlayer;
-                DOMInput.textDraw.textContent = 'The Winner is ';
+        updateAI: () => {   // when X vs AI
+            const activePlayer = 'AI';
+            DOMInput.O.textContent = activePlayer;
+            // highlight both because AI makes move very fast, toggle will not be visible
+            DOMInput.X.classList.add('active');
+            DOMInput.O.classList.add('active');
+        },
 
-                // 2-render congratulations message
-                DOMInput.textDraw.style.color = 'rgba(236,226,29,1)';
-                DOMInput.winWindow.style.display = 'block';
-            //}
+        renderWinner: (activePlayer) => {
+            // 1-upadate the activePlayer in the html 
+            DOMInput.winner.textContent = activePlayer;
+            DOMInput.textDraw.textContent = 'The Winner is ';
+
+            // 2-render congratulations message
+            DOMInput.textDraw.style.color = 'rgba(236,226,29,1)';
+            DOMInput.winWindow.style.display = 'block';
         },
 
         renderDraw: () => {
-            // check if any msg is already being displaced on the screen
-            //if(!anyActiveMsg()) {--- testing
-                // 1-update the activePlayer and message
-                DOMInput.winner.textContent = '';
-                DOMInput.textDraw.textContent = 'Draw!';
-                
-                // 2-render congratulations message
-                DOMInput.textDraw.style.color = '#222';
-                DOMInput.winWindow.style.display = 'block';
-            //}
+            // 1-update the activePlayer and message
+            DOMInput.winner.textContent = '';
+            DOMInput.textDraw.textContent = 'Draw!';
+            
+            // 2-render congratulations message
+            DOMInput.textDraw.style.color = '#222';
+            DOMInput.winWindow.style.display = 'block';
         },
 
         isMsgDisplayed : () => anyActiveMsg(),
@@ -210,20 +289,21 @@ const controller = ((UICtrl, logicCtrl) => {
     };
 
     // re-initailize the game
-    const reset = () => {    
-        // clearing the matrix data
-        logicCtrl.resetDS();
+    const reset = () => {            
+        logicCtrl.resetDS();    // clearing the matrix data
 
-        // clearing the UI / reset
-        UICtrl.resetUI(logicCtrl.getPlayerActive());
+        UICtrl.resetUI(logicCtrl.getPlayerActive());    // clearing the UI / reset
+
+        // check if vsAI flag is true then setup the game according to the X vs AI
+        if(logicCtrl.vsAI) {
+            UICtrl.updateAI();
+        }
     };
 
     const toggleUser = () => {
-        // 1-change active player
-        logicCtrl.toggleActive();
+        logicCtrl.toggleActive();   // 1-change active player
 
-        // 2-update active player on UI
-        UICtrl.updateActivePlayer(logicCtrl.getPlayerActive());        
+        UICtrl.updateActivePlayer();   // 2-update active player on UI    
     };
 
     // it provide a 2 sec delay to display the winner or draw message
@@ -234,9 +314,8 @@ const controller = ((UICtrl, logicCtrl) => {
     };
 
     const getUserInput = (event) => {
-        const field = event.target; //which space is selected
-        const value = field.textContent;  // is it X or O
-        let result = false;
+        const field = event.target; // which space is selected
+        const value = field.textContent;  // if it is already selected then value will be X or O
 
         // Check matrix ,if the required box is empty and if any message is being displayed 
         if(value === ' ' && !UICtrl.isMsgDisplayed()){
@@ -244,43 +323,43 @@ const controller = ((UICtrl, logicCtrl) => {
             logicCtrl.userInput(field.className);
 
             // 3-Update UI
-            UICtrl.updateField(event.target, logicCtrl.getPlayerActive())
+            UICtrl.updateField(event.target, logicCtrl.getPlayerActive());
 
             // 4-check if the player won the match
-            result = check();
+            check();
 
-            // 5-if matrix is full  
-            if(logicCtrl.isFull() && !result) {
-                // 1-render a draw msg
-                console.log('draw!');
-                UICtrl.renderDraw();
+            // 5-change active user-- if(AI) else just change active user
+            if(logicCtrl.vsAI == true && !UICtrl.isMsgDisplayed()) {
+                logicCtrl.toggleActive();   // change user to AI (make changes to DataStructure only)
+                
+                const coords = logicCtrl.aiPlayer();   // AI makes its move
+                const field = document.querySelector(`.element--${coords.row + 1}__${coords.col + 1}`);
+                UICtrl.updateField(field, logicCtrl.getPlayerActive());
 
-                // 2-reset UI
-                clearMsgWin();
+                check('AI');
+
+                logicCtrl.toggleActive();   // change AI to user (make changes to DataStructure only)
+            } 
+            else {
+                toggleUser();      // pass the control to the next play
             }
-
-            // 6-change active user
-            toggleUser();
         }
     };
 
-    const check = () => {
-        // Inspect Data Structure
-        const result = logicCtrl.checkMatrix();
-        
-        // if active user does't won, return undefined
-        if(result){ 
-            // Render Winner on the page
-            UICtrl.renderWinner(logicCtrl.getPlayerActive());
-            console.log(`${logicCtrl.getPlayerActive()} is the winner!`);
-    
-            // display winner window for 2 second then reset UI
-            clearMsgWin(); 
-
-            // return true to handle the case when the game won and the whole matrix is also filled
-            return true;
+    const check = (player = logicCtrl.getPlayerActive()) => {
+        const result = logicCtrl.checkWinner();    // checkWinner Inspect's Data Structure, if active user does't won, return undefined
+        if(result){
+            
+            UICtrl.renderWinner(player);    // Render Winner on the page
+            
+            clearMsgWin();     // wait for 2 second then reset UI
         }
-        return false;
+        else if(logicCtrl.isFull()) {    //  if matrix is full
+            
+            UICtrl.renderDraw();    // 1-render a draw msg
+            
+            clearMsgWin();   // 2-reset UI
+        }
     };
 
     return {
