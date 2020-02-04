2676c0e31ee5825a1ad95a47860b2210be31bde2
Davinder Singh <davindersaini003@gmail.com>
2020-02-04 14:30:18 +0530

    final

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 const logicController = (() => {
     const matrix = [new Array(3),new Array(3),new Array(3)];
     let playerActive = 'X';
-    let vsAI = true;  // flag about if the game is-- player vs AI
+    let vsAI = true;  // if playing against AI--> (player vs AI) or not
 
     const scrapeData = (data) => {
         let row, col, temp;
@@ -22,27 +22,27 @@ const logicController = (() => {
     };
 
     const checkMatrix = (curMatrix = matrix) => {
-        // 1-check rows
+        // 1) check rows
         for(let i = 0; i < 3; i++) {
             if(curMatrix[i][0] === curMatrix[i][1] && curMatrix[i][1] === curMatrix[i][2]) {
                 if(curMatrix[i][0] !== ' ') return curMatrix[i][0];
             }
         }
-        // 2-check cols
+        // 2) check cols
         for(let i = 0; i < 3; i++) {
             if(curMatrix[0][i] === curMatrix[1][i] && curMatrix[1][i] === curMatrix[2][i]) {
                 if(curMatrix[0][i] !== ' ') return curMatrix[0][i];
             }
         }
-        // 3-check diagonals
+        // 3) check diagonals
         if((curMatrix[0][0] === curMatrix[1][1] && curMatrix[1][1] === curMatrix[2][2])||(curMatrix[0][2] === curMatrix[1][1] && curMatrix[1][1] === curMatrix[2][0])) {
             if(curMatrix[1][1] !== ' ') return curMatrix[1][1];                   
         }
-        // 4-No winnig condition
+        // 4) No winnig condition
         return undefined;
     };
 
-    const getAvailFields = (currentMatrix) => {
+    const getAvailFields = (currentMatrix) => {     // using these AI will test all moves
         let emptyFields = []; // ex- [{row,col},{rol,col}]
         for(let i = 0; i < 3; i++){
             for(let j = 0; j < 3; j++){
@@ -56,10 +56,10 @@ const logicController = (() => {
 
     ///// "MINIMAX ALGO is used..." let O- aiPlayer AND X- humanPlayer
     const minimax = (newMatrix, player = 'O') => {
-        // check if there are empty fields in Matrix and return index of fields empty ex- [{row,col},{rol,col},...]
+        // check if there are empty fields in Matrix and return index of fields empty ex- [{row,col},{rol,col}]
         const availFields = getAvailFields(newMatrix);
 
-        //check if any of the player's won(if AI won return 10, otherwise -10) or if it is tie(0)
+        //check if any of the player's won(if AI won return 10, otherwise -10) or if it is tie
         if(checkMatrix(newMatrix) === 'O') {
             return { score: 10 };
         } else if(checkMatrix(newMatrix) === 'X') {
@@ -87,7 +87,7 @@ const logicController = (() => {
             }
 
             newMatrix[move.index.row][move.index.col] = ' ';    // resets newMatrix to what it was before
-            
+    
             moves.push(move);   // storing the results of current move, for comparing and finding best one  
         }
 
@@ -112,30 +112,28 @@ const logicController = (() => {
                 }
             }
         }
-
         return moves[bestMove];    // returning the best move of current Matrix to the previous states matrix or simply return back to one level up
     };
 
-    return {
+    return{
             getAI: () => vsAI,
             setAI: (state) => { vsAI = state },
             
             getPlayer: () => playerActive,
             setPlayer: (state) => { playerActive = state },
             
-            togglePlayer:  () => { playerActive = playerActive === 'O' ? 'X' : 'O'; },  
-            
             checkWinner: () => checkMatrix(),   // check if Active player is the winner
+            togglePlayer:  () => { playerActive = playerActive === 'O' ? 'X' : 'O'; },   // Active Player
 
-            resetDS: () => {     // Initialize matrix
+            resetDS: () => {    // Initialize matrix
                 for(let i = 0; i < 3; i++){
                     for(let j = 0; j < 3; j++){
                         matrix[i][j] = ' ';
                     }
                 }
-            },
+            },    
 
-            isMatrixFull: () => {   // check if the matrix is fully filled
+            isMatrixFull: () => {     // check if the matrix is fully filled
                 for(let i = 0; i < 3; i++){
                     for(let j = 0; j < 3; j++){
                         if(matrix[i][j] === ' ') {
@@ -144,9 +142,8 @@ const logicController = (() => {
                     }
                 }
                 return true;
-            },
-
-            // Player making move
+            },   
+            
             userMove: (dataString) => {
                 let data;
                 // scraping the required data from the dataString
@@ -155,7 +152,6 @@ const logicController = (() => {
                 matrix[data.row][data.col] = playerActive;
             },
 
-            // ai making move
             aiMove: () => {
                 let move = {};    //   {score:number, index:{row, col}}
                 move = minimax(matrix);
@@ -201,7 +197,7 @@ const UIController = (() => {
         getDOMInput: () => DOMInput,
     
         // this check if there is any msg being displayed on the screen, return's true or false
-        isMsgDisplayed : () => {  return DOMInput.window.style.display === 'block'; },
+        isMsgDisplayed : () => DOMInput.window.style.display === 'block',
 
         resetUI : (activePlayer) => {   // reset the game's UI
             // clean the Matrix
@@ -216,21 +212,22 @@ const UIController = (() => {
             DOMInput.window.style.display = 'none';
         },
         
-        updateField : (field, player) => {   // updating UI against user selection
+        updateField : (field, player) => {   // updating UI against user select
             field.textContent = player;
         },
 
         updateActivePlayer : () => {   // when X vs O
-            // when new game start ex- X vs O after a match of ex- X vs AI, the textContent ('AI') have to revert back to 'O'
+            // when new game start ex- X vs O after a match of ex- X vs AI, the textContent ('AI') have to revert back to O
             DOMInput.O.textContent = 'O';
-            // highlight the active player
+            // change(highlight) the active player
             DOMInput.X.classList.toggle('active');
             DOMInput.O.classList.toggle('active');
         },
 
         updateAI: () => {   // when X vs AI
-            DOMInput.O.textContent = 'AI';
-            // highlight both (because AI makes move very fast, toggle will not be visible)
+            const activePlayer = 'AI';
+            DOMInput.O.textContent = activePlayer;
+            // highlight both because AI makes move very fast, toggle will not be visible
             DOMInput.X.classList.add('active');
             DOMInput.O.classList.add('active');
         },
@@ -261,70 +258,60 @@ const UIController = (() => {
 //*************** GLOBAL APP CONTROLLER *************//
 //***************************************************//
 const controller = ((UICtrl, logicCtrl) => { 
-    
+
     const setupEventListeners = () => {
         const DOM = UICtrl.getDOMInput();
-
         const fieldArr = Object.values(DOM.field);   // converting object to array
-        fieldArr.forEach( el => {   // setting listeners to all the selectable fields in t-t-t matrix
+        
+        fieldArr.forEach( el => {   // setting up listener's on all input fields in the matrix
             el.addEventListener('click', getUserInput);
         });
 
-        DOM.setPanel.style.display = 'none';    // set display property of the setting panel to none(to default)
+        DOM.setPanel.style.display = 'none';    // hide the setting panel (default)
 
-        // when user click the settings icon
-        //pending when user click outside of the panel it should be closed automatically
-        DOM.setting.addEventListener('click', () =>{
+        DOM.setting.addEventListener('click', () =>{    // when user click the settings icon
             DOM.setPanel.style.display = DOM.setPanel.style.display === 'none' ? 'block' : 'none';
         });
 
         // listen to changes in setting
         DOM.btn_Human.addEventListener('click', () => {     // human vs human
-            // 1-turn off the AI flag
-            logicCtrl.setAI(false);
+            logicCtrl.setAI(false);                         // 1-turn off the AI flag
+ 
+            UICtrl.updateActivePlayer();                    // 2-set the second player as the 'O'
+            reset();                                        // 3-reset the game
 
-            // 2-update UI and set the second player as the 'O'
-            UICtrl.updateActivePlayer();
-            
-            // 3-reset the game
-            reset();
-            
-            // 4-hide the setting panel again
-            DOM.setPanel.style.display = 'none';
+            DOM.setPanel.style.display = 'none';            // 4-hide the setting panel again
         });
-        
+
         DOM.btn_AI.addEventListener('click', () => {    // human vs AI
-            // 1. turn on the AI flag
-            logicCtrl.setAI(true);
+            logicCtrl.setAI(true);                      // 1-turn off the AI flag
             
-            // 2. reset will check, if the vsAI flag is on then setup the game accordingly
-            reset();
-            
-            // 3. hide the setting panel again
-            DOM.setPanel.style.display = 'none';
+            reset();                                    // 2-reset the game
+            AIMove();                                   // 3-set active player to 'X', so that correct entry is displayed, as user will play first
+
+            DOM.setPanel.style.display = 'none';        // 4-hide the setting panel again
         });
     };
-    
+
     const AIMove = () => {
-        // 1-AI makes its move and update the DataStructure- return cordinates of the field
-        const cords = logicCtrl.aiMove();
-
-        // 2-render selection on UI
-        const field = document.querySelector(`.element--${cords.row + 1}__${cords.col + 1}`);
-        UICtrl.updateField(field, logicCtrl.getPlayer());
-
-        // 3-check if the AI won the match 
-        check('AI');
-
-        // 4-pass control to the user
-        logicCtrl.togglePlayer();   
-    };
+        if(logicCtrl.getAI() === true && logicCtrl.getPlayer() === 'O') {
+             // 1- AI makes its move
+            const cords = logicCtrl.aiMove();
+            const field = document.querySelector(`.element--${cords.row + 1}__${cords.col + 1}`);
+            // 2-render change over UI
+            UICtrl.updateField(field, logicCtrl.getPlayer());
+            // 3-check if AI won
+            check('AI');
+            // 4-change Active player (pass control to the player 'X')
+            logicCtrl.togglePlayer();
+        }
+    }
 
     const getUserInput = (event) => {
         const field = event.target; // which space is selected
         const value = field.textContent;  // if it is already selected then value will be X or O
 
-        // 1-Check matrix ,if the required box is empty and if any message is being displayed 
+        // Check matrix ,if the required box is empty and if any message is being displayed 
         if(value === ' ' && !UICtrl.isMsgDisplayed()){
             // 2-Update DS matrix
             logicCtrl.userMove(field.className);
@@ -346,41 +333,46 @@ const controller = ((UICtrl, logicCtrl) => {
             } 
             else {   // pass the control to other player - for human vs human
                 UICtrl.updateActivePlayer();   // 1-update active player on UI
-            }
+            }            	
         }
     };
 
     // re-initailize the game
     const reset = () => {            
         logicCtrl.resetDS();    // clearing the matrix data
-
-        UICtrl.resetUI(logicCtrl.getPlayer());    // clearing the UI / reset and set active player
+        UICtrl.resetUI(logicCtrl.getPlayer());    // clearing the UI / reset
 
         // check if vsAI flag is true then setup the game according to the X vs AI
         if(logicCtrl.getAI()) {
-            logicCtrl.setPlayer('X');   // set active player to 'X', as user will make first move
-            UICtrl.updateAI();     // render 'AI' over screen
+            UICtrl.updateAI();
         }
     };
 
     // it provide a 2 sec delay to display the winner
-    const clearMsgWin = () => {
+    const clearMsg = (draw) => {
+        let isDraw = draw;   // in case, the last move before a draw is made by 'X', then after draw, AI makes move
         setTimeout(() => {
             reset();
+            if(isDraw === true) { 
+                AIMove();
+            }
         }, 2000);
     };
 
     const check = (player = logicCtrl.getPlayer()) => {
         const result = logicCtrl.checkWinner();    // checkWinner Inspect's Data Structure, if active user does't won, return undefined
-        if(result){
+        let isDraw = false;     // this false is used to distinguish between -> a draw and a game won in the last move (required for AI to make the first move in a new game)
 
+        if(result){
             UICtrl.renderWinner(player);    // Render Winner on the page
-            clearMsgWin();     // wait for 2 second then reset UI
+            
+            clearMsg(isDraw);     // wait for 2 second then reset UI
         }
         else if(logicCtrl.isMatrixFull()) {    //  if matrix is full
+            UICtrl.renderDraw();    // 1-render a draw msg
             
-            UICtrl.renderDraw();    // Render a draw msg
-            clearMsgWin();
+            isDraw = true;
+            clearMsg(isDraw);   // 2-reset UI
         }
     };
 
